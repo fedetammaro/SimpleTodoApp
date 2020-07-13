437fb6f9a3d9db8b476d042ce1d18796b3cdbbf5
Federico <dev.sfullez@gmail.com>
2020-07-13 11:21:34 +0200

    Added comments to Task unit tests [ci skip]

--- src/test/java/it/unifi/simpletodoapp/model/TaskTest.java
+++ src/test/java/it/unifi/simpletodoapp/model/TaskTest.java
@@ -17,42 +17,52 @@ public class TaskTest {
 
 	@Test
 	public void testTaskEqualityWithItself() {
+		// Exercise and verify phases (no setup phase required)
 		assertThat(task).isEqualTo(task);
 	}
 
 	@Test
 	public void testTaskEqualityWithNull() {
+		// Exercise and verify phases (no setup phase required)
 		assertThat(task).isNotEqualTo(null);
 	}
 
 	@Test
 	public void testTaskEqualityWithOtherClass() {
+		// Exercise and verify phases (no setup phase required)
 		assertThat(task).isNotEqualTo("String object");
 	}
 
 	@Test
 	public void testTaskEqualityWithDifferentId() {
+		// Setup phase
 		Task anotherTask = new Task("2", "Buy groceries");
 		
+		// Exercise and verify phases
 		assertThat(task).isNotEqualTo(anotherTask);
 	}
 
 	@Test
 	public void testTaskEqualityWithDifferentDescription() {
+		// Setup phase
 		Task anotherTask = new Task("1", "Start using TDD");
 		
+		// Exercise and verify phases
 		assertThat(task).isNotEqualTo(anotherTask);
 	}
 
 	@Test
 	public void testTaskEqualityWithEqualTask() {
+		// Setup phase
 		Task anotherTask = new Task("1", "Buy groceries");
 		
+		// Exercise and verify phases
 		assertThat(task).isEqualTo(anotherTask);	
 	}
 
 	@Test
 	public void testTaskHashCode() {
+		// Exercise and verify phases (no setup phase required)
 		assertThat(task.hashCode()).isEqualTo(
 				Objects.hash(task.getId(), task.getDescription())
 				);
