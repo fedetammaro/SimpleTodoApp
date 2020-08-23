f5b3ef5d9dcaf6dd8d88410147309534baf6cc72
Federico <dev.sfullez@gmail.com>
2020-08-23 10:04:50 +0200

    Enforced primary and secondary roles in init.js [ci skip]

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ configuration = {
 	"members": [
 		{
 			"_id": 0,
-			"host": "mongo-primary:27017"
+			"host": "mongo-primary:27017",
 		},
 		{
 			"_id": 1,
@@ -13,3 +13,12 @@ configuration = {
 }
 
 rs.initiate(configuration)
+
+/* Force primary and secondary role for rare cases when it doesn't
+ * happen by default: rs.reconfig() is executed only if mongo-secondary
+ * has the PRIMARY role and needs to reconfigure the replica set */
+replica_configuration = rs.conf()
+replica_configuration.members[0].priority = 1
+replica_configuration.members[1].priority = 0.5
+
+rs.reconfig(replica_configuration)
